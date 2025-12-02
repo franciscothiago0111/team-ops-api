0e18ccaa5ed2247092e8760bdb6fa2a3b2133fec
franciscothiago0111 <franciscothiago0111@gmail.com>
2025-12-02 09:35:07 -0300

    Implement authentication module with controller and service; add tests for AuthController and AuthService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,6 +12,7 @@ import { DatabaseModule } from './database/database.module';
     }),
     UsersModule,
     DatabaseModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
