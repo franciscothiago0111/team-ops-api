69256bf7675f58c2aad10f1bdb5fb782e84ef9cc
franciscothiago0111 <franciscothiago0111@gmail.com>
2025-12-08 12:33:33 -0300

    refactor(task-file-upload): remove file validation and logging from upload method

--- src/tasks/task-files/task-file-upload/task-file-upload.controller.ts
+++ src/tasks/task-files/task-file-upload/task-file-upload.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Controller,
   Param,
   Post,
@@ -29,12 +28,6 @@ export class TaskFileUploadController {
     @Param('id') taskId: string,
     @UploadedFiles() files: Express.Multer.File[],
   ) {
-    console.log('Uploaded files:', files);
-
-    if (!files || files.length === 0) {
-      throw new BadRequestException('At least one file is required');
-    }
-
     const result = await this.taskFileUploadService.run(taskId, files);
 
     return this.response.success({
